61846051032d2318d49bb99370054518112e95bb
hatibovicvedad <hatibovicvedad12@gmail.com>
2025-05-07 20:49:34 +0200

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,102 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "vedoserver"; # Define your hostname.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Sarajevo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "bs_BA.UTF-8";
+    LC_IDENTIFICATION = "bs_BA.UTF-8";
+    LC_MEASUREMENT = "bs_BA.UTF-8";
+    LC_MONETARY = "bs_BA.UTF-8";
+    LC_NAME = "bs_BA.UTF-8";
+    LC_NUMERIC = "bs_BA.UTF-8";
+    LC_PAPER = "bs_BA.UTF-8";
+    LC_TELEPHONE = "bs_BA.UTF-8";
+    LC_TIME = "bs_BA.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vedo = {
+    isNormalUser = true;
+    description = "vedad";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # SAMBA START
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    shares = {
+      sharebigboi = {
+        path = "/mnt";
+        browseable = true;
+        writable = true;
+        guestOk = true;
+      };
+    };
+  };
+  # SAMBA END
+
+  # Docker Setup
+  virtualisation.docker.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "24.11";
+
+}
