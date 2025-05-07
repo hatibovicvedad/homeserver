17f62de90396c38743d3bbe1a4c7b40bf28d92d3
hatibovicvedad <hatibovicvedad12@gmail.com>
2025-05-07 21:15:13 +0200

    Update configuration.nix

--- nixos-basic-config/configuration.nix
+++ nixos-basic-config/configuration.nix
@@ -57,6 +57,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+  docker-compose
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
