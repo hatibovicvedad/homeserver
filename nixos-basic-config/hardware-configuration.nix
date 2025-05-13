04bf2c4367c6f6b400b4ccdaf63467cd6cb0a47a
hatibovicvedad <hatibovicvedad12@gmail.com>
2025-05-13 20:09:49 +0200

    Update hardware-configuration.nix

--- nixos-basic-config/hardware-configuration.nix
+++ nixos-basic-config/hardware-configuration.nix
@@ -18,9 +18,15 @@
 
 # MOUNT START
 
-# mnt
-  fileSystems."/mnt" =
+# ssd
+  fileSystems."/mnt/ssd" =
     { device = "/dev/disk/by-uuid/8c8620fe-70be-488b-bb4e-b96235cd43aa";
+      fsType = "ext4";
+    };
+
+# hdde
+  fileSystems."/mnt/hdd" =
+    { device = "/dev/disk/by-uuid/0c821c31-8456-4614-85b3-fe7dae18f61b";
       fsType = "ext4";
     };
 
